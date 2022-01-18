d38939c63c8c16de889438303ac8c7f694dee8fd
Jake <jacob.lindsey88@gmail.com>
2022-01-18 14:27:34 -0600

    Added separate dark/light themes and wired up the toggle switch for said themes.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,7 +5,7 @@ const Form = () => {
     return (
         <section className="container is-max-desktop">
             <div className="field">
-                <label className="label has-text-left has-text-light has-text-weight-light">Email</label>
+                <label className="label has-text-left has-text-weight-light">Email</label>
                 <div className="control has-icons-left has-icons-right">
                     <input className="input" type="email" placeholder="Email Address" />
                     <span className="icon is-small is-left">
@@ -15,7 +15,7 @@ const Form = () => {
             </div>
 
             <div className="field">
-                <label className="label has-text-left has-text-light has-text-weight-light">Message</label>
+                <label className="label has-text-left has-text-weight-light">Message</label>
                 <div className="control">
                     <textarea className="textarea" placeholder="Leave me a message" />
                 </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,17 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
 
     const [expanded, setExpanded] = useState(false);
+    const [darkMode, setDarkMode] = useState(true);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        let theme = darkMode ? "light" : "dark";
+        document.documentElement.classList.add('theme-transition');
+        document.documentElement.setAttribute("data-theme", theme);
+        window.setTimeout(function() {
+            document.documentElement.classList.remove('theme-transition')
+        }, 1000);
+    };
 
     return (
         <nav className="navbar is-flex is-align-items-center is-transparent">
@@ -11,7 +22,7 @@ const Navbar = () => {
                 <a href="/" className="navbar-item">
                     <span className={styles.logo}>
                         <span>J<span className={styles.mirroredLetter}>J</span></span>
-                        <div className="has-text-light is-size-7-mobile is-size-6 is-hidden-mobile">JACOB LINDSEY</div>
+                        <div className="is-size-7-mobile is-size-6 is-hidden-mobile">JACOB LINDSEY</div>
                     </span>
                 </a>
             </div>
@@ -28,11 +39,20 @@ const Navbar = () => {
                 <span className={styles.iconBar}></span>
             </button>
             <div id={styles.navbarMenu} className="navbar-menu" aria-labelledby={styles.navbarToggle}>
-                <ul className="navbar-end">
-                    <li className="navbar-item has-text-light">HOME</li>
-                    <li className="navbar-item has-text-light">ABOUT</li>
-                    <li className="navbar-item has-text-light">CONTACT</li>
-                    <li className="navbar-item has-text-light">PORTFOLIO</li>
+                <ul className="navbar-end is-flex">
+                    <li className="navbar-item">
+                        <div className="field is-flex">
+                            <input id="switchRoundedInfo" type="checkbox" name="light-dark-toggle" className="switch is-rounded is-info" onChange={toggleDarkMode} checked={!!darkMode} />
+                            <label htmlFor="switchRoundedInfo">
+                                D
+                            </label>
+                            <span className="mr-2">L</span>
+                        </div>
+                    </li>
+                    <li className="navbar-item">HOME</li>
+                    <li className="navbar-item">ABOUT</li>
+                    <li className="navbar-item">CONTACT</li>
+                    <li className="navbar-item">PORTFOLIO</li>
                 </ul>
             </div>
         </nav>
