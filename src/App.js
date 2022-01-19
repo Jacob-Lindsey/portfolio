269583fae63e576f3b9fb4aa7f8fa68df289e272
Jake <jacob.lindsey88@gmail.com>
2022-01-19 13:55:02 -0600

    Set up basic routing.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
+import Bio from './components/Bio/Bio';
 import Form from './components/Form/Form';
+import Header from './components/Header/Header';
+import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
+
   return (
     <div className="App">
-      <Header />
-      <Form />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="bio" element={<Bio />} />
+          <Route path="contact" element={<Form />} />
+          <Route path="portfolio" element={<Portfolio />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import useStickyState from "../../utils/useStickyState";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
 
     const [expanded, setExpanded] = useState(false);
-    const [darkMode, setDarkMode] = useState(true);
+    const [theme, setTheme] = useStickyState('dark', 'theme');
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme);
+    });
 
     const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-        let theme = darkMode ? "light" : "dark";
+        let preferredTheme = theme === "dark" ? "light" : "dark";
+        setTheme(preferredTheme);
         document.documentElement.classList.add('theme-transition');
-        document.documentElement.setAttribute("data-theme", theme);
+        document.documentElement.setAttribute("data-theme", preferredTheme);
         window.setTimeout(function() {
             document.documentElement.classList.remove('theme-transition')
         }, 1000);
@@ -38,21 +44,18 @@ const Navbar = () => {
                 <span className={styles.iconBar}></span>
                 <span className={styles.iconBar}></span>
             </button>
-            <div id={styles.navbarMenu} className="navbar-menu" aria-labelledby={styles.navbarToggle}>
+            <div id={styles.navbarMenu} className="navbar-menu is-justify-content-center" aria-labelledby={styles.navbarToggle}>
                 <ul className="navbar-end is-flex">
                     <li className="navbar-item">
-                        <div className="field is-flex">
-                            <input id="switchRoundedInfo" type="checkbox" name="light-dark-toggle" className="switch is-rounded is-info" onChange={toggleDarkMode} checked={!!darkMode} />
-                            <label htmlFor="switchRoundedInfo">
-                                D
-                            </label>
-                            <span className="mr-2">L</span>
+                        <div className="field is-flex is-justify-content-center">
+                            <input id="switchRoundedInfo" type="checkbox" name="light-dark-toggle" className="switch is-rounded is-info" onChange={toggleDarkMode} checked={theme === "dark" ? true : false} />
+                            <label htmlFor="switchRoundedInfo"></label>
                         </div>
                     </li>
-                    <li className="navbar-item">HOME</li>
-                    <li className="navbar-item">ABOUT</li>
-                    <li className="navbar-item">CONTACT</li>
-                    <li className="navbar-item">PORTFOLIO</li>
+                    <li className="navbar-item is-clickable" onClick={() => setExpanded(false)}><Link to="/">HOME</Link></li>
+                    <li className="navbar-item is-clickable" onClick={() => setExpanded(false)}><Link to="bio">BIO</Link></li>
+                    <li className="navbar-item is-clickable" onClick={() => setExpanded(false)}><Link to="contact">CONTACT</Link></li>
+                    <li className="navbar-item is-clickable" onClick={() => setExpanded(false)}><Link to="portfolio">PORTFOLIO</Link></li>
                 </ul>
             </div>
         </nav>
